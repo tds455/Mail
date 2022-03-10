81e479c077ca63a6354ab4e0a0c30b9853c222bf
tds455 <tds-1993@outlook.com>
2022-03-10 23:47:30 +0900

    Finished mailbox function, added open_email function, updated css

--- mail/mail/static/mail/inbox.js
+++ mail/mail/static/mail/inbox.js
@@ -29,26 +29,71 @@ function load_mailbox(mailbox) {
   document.querySelector('#emails-view').style.display = 'block';
   document.querySelector('#compose-view').style.display = 'none';
 
-  // Show the mailbox name
+  // When a mailbox is visited, the name of the mailbox should appear at the top of the page
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
 
-  // Retrieve mailbox content using GET request
+
+
+
+  // When a mailbox is visited, the application should first query the API for the latest emails in that mailbox.
   // Based on sample code in CS50W Mail specification, adjusted to reflect which mailbox the user is trying to access
   // After retrieving, store each e-mail in it's own div (https://www.tutorialspoint.com/how-to-add-a-new-element-to-html-dom-in-javascript) and 50W Mail hints section, using a for loop
   // for looping through an array in JS - forEach https://www.w3schools.com/jsref/jsref_foreach.asp
-  // Note - Use `` and not '' when making fetch request
+  // Note - Use `` and not '' when making fetch request and writing HTML, but use '' when indexing the objects inside of that code.
   fetch(`/emails/${mailbox}`)
   .then(response => response.json())
   .then(emails => {
     emails.forEach(email => {
+    // Each email should then be rendered in its own box (e.g. as a <div> with a border) that displays who the email is from, what the subject line is, and the timestamp of the email.
     const element = document.createElement('div');
-    // Render the sender, recipients, subject, timestamp and body to user
-    element.innerHTML = "<p>testing hello</p>";
+    // Show the sender, subject and timestamp of each email
+    // https://developer.mozilla.org/en-US/docs/Web/API/Element/classList
+    element.classList.add("emails");
+    // element.classList.add("row");
+    element.innerHTML = `<p class="col-sm">Sender: ${email['sender']}</p>  <p class="col-sm">Subject: ${email['subject']}</p> <p class="col-sm">Date and time: ${email['timestamp']}</p>`
+    // If the email is unread, it should appear with a white background. If the email has been read, it should appear with a gray background.
+    // https://www.codegrepper.com/code-examples/javascript/javascript+add+style+to+div
+    if (email['read'] == 0) {
+      element.style.backgroundColor = "white";
+    }
+    else {
+      element.style.backgroundColor = "lightgrey";
+    }
+
+    // Make e-mail "clickable"
+    element.addEventListener('click', event => {open_email(email['id'])});
+
+    // Render the e-mails back to the user
     document.querySelector('#emails-view').appendChild(element);
     });
 
   });
   }
+
+function open_email(id) {
+  // Hide other views
+  // Show the mailbox and hide other views
+  document.querySelector('#emails-view').style.display = 'block';
+  document.querySelector('#compose-view').style.display = 'none';
+
+  //  make a GET request to /emails/<email_id> to request the email.
+  fetch(`/emails/${id}`)
+  .then(response => response.json())
+  .then(email => {
+
+      // show the email’s sender, recipients, subject, timestamp, and body.
+      const element = document.createElement('div');
+      element.classList.add("emails");
+      element.innerHTML = `<p class="col-sm">Sender: ${email['sender']}</p>  <p class="col-sm">Subject: ${email['recipients']}</p>  <p class="col-sm">Subject: ${email['subject']}</p> <p class="col-sm">Date and time: ${email['timestamp']} <p class="col-sm">Date and time: ${email['body']}</p>`
+      document.querySelector('#emails-view').appendChild(element);
+
+      // mark the email as read.
+  });
+  
+
+
+}
+
 
 function send_mail() {
   
